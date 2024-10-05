e3a6dfb805a038a8c3e6b927efed435a191fcb84
vishal <vishrrajput02@gmail.com>
2024-10-05 16:20:18 +0530

    Added the rate limiter and Added the Proxy implementation for Booking and Flight Service

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 const express = require('express');
+const rateLimit=require('express-rate-limit')
+const {createProxyMiddleware}=require('http-proxy-middleware')
 
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
+const limiter = rateLimit({
+	windowMs: 2 * 60 * 1000, // 2 minutes
+	limit: 3, // Limit each IP to 3 requests per `window` (here, per 15 minutes).
+	
+})
+const proxyroutingflight=createProxyMiddleware({
+    target: ServerConfig.FLIGHT_SERVICE,
+    changeOrigin: true,
+})
+const proxyroutingbooking=createProxyMiddleware({
+    target: ServerConfig.BOOKING_SERVICE,
+    changeOrigin: true,
+})
+
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
+app.use(limiter);
+app.use('/flightsService',proxyroutingflight);
+app.use('/bookingService',proxyroutingbooking);
 app.use('/api', apiRoutes);
 
 app.listen(ServerConfig.PORT, () => {
